2dc1863881166a815133dbe2b97179800f3f10c8
JAI-KAI <good003047@gmail.com>
2025-01-16 07:23:16 +0800

    navbar

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-
+    <div>
+        kaku 2025
+    </div>
 </template>
 
 <script setup>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <nav>
+            <ul>
+                <li>
+                    <router-link to="/">首頁</router-link>
+                </li>
+                <li>
+                    <router-link to="/about">關於這個網站</router-link>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
