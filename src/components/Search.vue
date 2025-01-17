71eaf9d54bde6bd5c6793e396f4d77b43c55372d
JAI-KAI <good003047@gmail.com>
2025-01-17 16:40:08 +0800

    HomePage update

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
-        <input type="text">
+        <input type="text" v-model="userInput">
         <button @click="search">Search</button>
     </div>
 </template>
 
 <script setup>
+const userInput = defineModel();
 const emit = defineEmits(['search']);
 function search() {
     emit('search')
